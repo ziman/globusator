af509136be34a45dfb594dd5c8318e3aeca5e9f6
Matus Tejiscak <ziman@functor.sk>
2012-02-22 22:53:09 +0100

    Adjustable size, postfill removed.

--- slize.pl
+++ slize.pl
@@ -13,6 +13,8 @@ sub slizik {
 	my $quality = 95;
 	my $format = "JPEG c:$quality";
 	my $ptofile = "brno.pto";
+	my $srcw = 20079;
+	my $srch = 20079;
 
 	# Pitch and roll
 	my $pitch = 90; # city centre: equator/poles?
@@ -34,7 +36,7 @@ sub slizik {
 	open my $F, ">$ptofile";
 	print $F "p f6 w$imgw h$imgh v179 E0 R0 S$left,$right,$top,$bot n\"$format r:CROP\"\n";
 	print $F "m g1 i0 f0 m2 p0.00784314\n";
-	print $F "i w8000 h8000 f10 v292.152297783717 Ra0 Rb0 Rc0 Rd0 Re0 Eev0 Er1 Eb1 r$roll p$pitch";
+	print $F "i w$srcw h$srch f10 v292.152297783717 Ra0 Rb0 Rc0 Rd0 Re0 Eev0 Er1 Eb1 r$roll p$pitch";
 	print $F "  y$yaw TrX0 TrY0 TrZ0 j0 a0 b0 c0 d0 e0 g0 t0 Va1 Vb0 Vc0 Vd0 Vx0 Vy0 Vm5 n\"$infile\"\n";
 	print $F "v r0\nv p0\nv y0\nv\n";
 	close $F;
@@ -47,15 +49,15 @@ sub slizik {
 		. "-crop "
 			. ($right-$left)."x".($bot-$top)
 			. "+".$left."+".$top." "
-		. "-fill '#2c5573' -fuzz 5% -opaque black "
+#		. "-fill '#2c5573' -fuzz 5% -opaque black "
 		. "$outfile"
 	) == 0 or die "Could not postprocess image.";
 	
 	print "- OK\n";
 }
 
-my $outname = 'lr';
-my $srcfile = 'brno8000.jpg';
+my $outname = 'hr';
+my $srcfile = 'brno.jpg';
 my $height  = 3600;
 my $workers = 1;
 my $stripes = 12;
